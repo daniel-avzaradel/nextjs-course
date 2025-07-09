f6dff340e02d59ecb7c46bc9732eed7f8c8c3f12
Daniel <daniel.avzaradel@gmail.com>
2025-07-10 00:05:16 +0300

    server side vs client side components

---
+++ app/_components/ButtonComponent.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const ButtonComponent = () => {
+  return (
+    <button onClick={() => alert("Hello")}>ButtonComponent</button>
+  )
+}
+
+export default ButtonComponent
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,31 +1,20 @@
-import Image from 'next/image';
-import React from 'react'
+import ButtonComponent from '@/app/_components/ButtonComponent';
+import React from 'react';
 
+// Server Component: fetch data on the server
 const Contact = async () => {
-
-  const response = await fetch('https://pokeapi.co/api/v2/pokedex/kanto/');
-  const data = await response.json();
-  console.log(data.pokemon_entries);
+  // Add protocol to the fetch URL
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-      {data.pokemon_entries.map((el: any) => {
-      return (
-        <div key={el.entry_number} className="flex flex-col items-center bg-gray-800 rounded shadow p-4">
-        <h2 className="text-lg font-semibold mb-2 capitalize">{el.pokemon_species.name}</h2>
-        <Image
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${el.entry_number}.svg`}
-          alt={el.pokemon_species.name}
-          width={96}
-          height={96}
-          style={{ width: "100px", height: "120px" }}
-          priority
-        />
-        </div>
-      )
-      })}
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mx-4 my-8">
+      <p>Contact</p>
+      <ButtonComponent />
+      {/* Example: render number of posts fetched */}
+      <p className="col-span-full">Posts fetched: {posts.length}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
