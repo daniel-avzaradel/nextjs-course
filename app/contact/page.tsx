a8ef40bea88b96245d77e895d977ba637870b4f6
Daniel <daniel.avzaradel@gmail.com>
2025-07-09 23:32:59 +0300

    using copilot to fix code

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/image';
 import React from 'react'
 
-const Contact = () => {
+const Contact = async () => {
+
+  const response = await fetch('https://pokeapi.co/api/v2/pokedex/kanto/');
+  const data = await response.json();
+  console.log(data.pokemon_entries);
+
   return (
-    <div>Contact</div>
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+      {data.pokemon_entries.map((el: any) => {
+      return (
+        <div key={el.entry_number} className="flex flex-col items-center bg-gray-800 rounded shadow p-4">
+        <h2 className="text-lg font-semibold mb-2 capitalize">{el.pokemon_species.name}</h2>
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${el.entry_number}.svg`}
+          alt={el.pokemon_species.name}
+          width={96}
+          height={96}
+          style={{ width: "100px", height: "120px" }}
+          priority
+        />
+        </div>
+      )
+      })}
+    </div>
   )
 }
 
--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    // remotePatterns: [
-    //   {
-    //     hostname: "raw.githubusercontent.com"
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        hostname: "raw.githubusercontent.com"
+      },
+    ],
   }
 };
 
